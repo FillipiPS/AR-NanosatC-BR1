d94962a1f2b613147d0c8fabcf5e04de99da180c
FillipiPS <fillipi_ps@outlook.com>
2023-06-17 22:21:19 -0300

    Initial Commit

---
+++ AR-NanosatC-BR1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AR-NanosatC-BR1
+//
+//  Created by Fillipi Paiva Suszek on 06/12/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
